5f394833fbe36452cf0b5795a4ecb7713e7ac11e
Jake Bartha <60204257+jakebartha@users.noreply.github.com>
2020-02-07 12:16:39 -0500

    Add files via upload

---
+++ Part5.py
@@ -0,0 +1,17 @@
+letter_scores = {"a": 1, "e": 1, "i": 1, "o": 1, "u": 1, "l": 1, "n": 1, "r": 1, "s": 1, "t": 1, "d": 2, "g": 2, "b": 3, "c": 3, "m": 3, "p": 3,
+                 "f": 4, "h": 4, "v": 4, "w": 4, "y": 4, "k": 5, "j": 8, "x": 8, "q": 10, "z": 10,}
+tally = []
+def scrabble_score(word):
+    word = word.lower()
+    for i in word:
+        for key, value in letter_scores.items():
+            if i == key:
+                letter_scores.get(i)
+                tally.append(value)
+    return tally
+
+
+userWord = raw_input("Enter a word to be scored: ")
+scrabble_score(userWord)
+total_score = sum(tally)
+print (total_score)
