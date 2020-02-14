303726e8cd36786b869d794085a5076f68cec9ab
Jake Bartha <60204257+jakebartha@users.noreply.github.com>
2020-02-14 13:00:17 -0500

    Add files via upload
    could not get my command to run with sys.argv

---
+++ Codingchallenge3/argvfile.py
@@ -0,0 +1,9 @@
+import sys
+
+letter_scores = ['one', 'two', 'four']
+tally = []
+letter_scores.append(sys.argv[1])
+letter_scores.append(sys.argv[2])
+letter_scores.append(sys.argv[3])
+# couldn't get ys.argv to work therefore could not run. 
+
