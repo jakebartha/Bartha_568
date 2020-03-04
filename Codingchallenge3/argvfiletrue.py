990793b96ae69498bf880ba3dca5d91c9df56710
Jake Bartha <60204257+jakebartha@users.noreply.github.com>
2020-03-04 16:50:24 -0500

    Update argvfiletrue.py

--- Codingchallenge3/argvfiletrue.py
+++ Codingchallenge3/argvfiletrue.py
@@ -6,5 +6,6 @@ words.append(sys.argv[1])
 words.append(sys.argv[2])
 words.append(sys.argv[3])
 # couldn't get ys.argv to work therefore could not run.
+# system cant find the batch file can not use commands that internet recommends to search for this batfile.  
 for x in words:
     print ('there are' + str(len(x)) + str('this many letters') + str(x))
