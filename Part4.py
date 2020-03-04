d6b9011880f0b3b1028b0929fbb9a9a8b3235855
Jake Bartha <60204257+jakebartha@users.noreply.github.com>
2020-03-04 16:59:34 -0500

    Delete Part4.py

--- Part4.py
+++
@@ -1,9 +0,0 @@
-age = int(input("What is your age? "))
-if age < 65:
-    ret_age = 65 - age
-    print('you can retire in {0} years'.format(ret_age))
-
-else:
-    print("you are retired")
-else: # Rejection statement is the entered input is not a number
-    print "'" + age + "'" + " is not a number."
